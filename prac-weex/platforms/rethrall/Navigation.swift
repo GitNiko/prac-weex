2ee69f2660878fe226d6ace573a95510bb7a3c97
GitNiko <galaxis.ling@gmail.com>
2018-01-24 09:14:02 +0800

    can't not unregister exist font

--- prac-weex/platforms/rethrall/Navigation.swift
+++ prac-weex/platforms/rethrall/Navigation.swift
@@ -45,16 +45,30 @@ class Navigation: NSObject {
     }
     
     @objc func registFont(_ filePath:String) -> Bool {
+        // 如果字体已经注册过了，则无法注册，即使注销也会返回false，原因不明，推测是内字体加载后就被引用了且无法被回收。
+        // 目前只能不把字体放到app中，通过远程下载后加载指定路径下的字体。
         guard let provider = CGDataProvider(filename: filePath) else {
             return false
         }
         
         let font = CGFont(provider)
-        if let fc = CGFont.init(NSString.init(string: "iconfont.ttf")) {
-            _ = CTFontManagerUnregisterGraphicsFont(fc, nil)
-        }
+//        if let fc = CGFont.init(NSString.init(string: "iconfont")) {
+//            print(fc)
+//            let iconPath = Bundle.main.path(forResource: "iconfont", ofType: "ttf")
+////            let sss = CTFontManagerUnregisterGraphicsFont(fc, nil)
+//            let iconUrl = NSURL.init(fileURLWithPath: iconPath!)
+//            let error:UnsafeMutablePointer<Unmanaged<CFError>?>? = nil
+////            guard CTFontManagerUnregisterGraphicsFont(fc, error) else {
+////                let s = error?.pointee?.takeUnretainedValue()
+//////                let errordesc = CFErrorCopyDescription(error?.pointee?.takeUnretainedValue())
+//////                print(errordesc!)
+////                return false
+////            }
+//
+//        }
 //        let result = CTFontManagerRegisterFontsForURL(path, CTFontManagerScope.process, nil)
-        let result = CTFontManagerRegisterGraphicsFont(font, nil)
+        let registerError:UnsafeMutablePointer<Unmanaged<CFError>?>? = nil
+        let result = CTFontManagerRegisterGraphicsFont(font, registerError)
         return result
     }
     
